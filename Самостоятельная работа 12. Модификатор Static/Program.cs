1022eaa06528d5fb1c37d6b23130ce8c4458fe34
ShevtsovDenis <shevcov.organika@yandex.ru>
2022-11-13 20:29:22 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная работа 12. Модификатор Static/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_12.Модификатор_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            //Вызов методов определения длины и площади
+            double lenght = Round.GetLenght(r);
+            double square = Round.GetSquare(r);
+            Console.WriteLine($"Длина окружности ={lenght:f2} \nПлощадь окружности ={square:f2}");
+            Console.Write("Введите координату Х=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату У=");
+            double y = Convert.ToDouble(Console.ReadLine());
+            //Вызов метода определения принадлежности
+            string affiliation = Round.GetAffiliation(x, y, r);
+            Console.WriteLine($"{affiliation}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Самостоятельная работа 12. Модификатор Static/Сircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_12.Модификатор_Static
+{
+    static class Round
+    {
+        //Метод нахождения длины окружности
+        static public double GetLenght(double r)
+        {
+            return 2 * Math.PI * Math.Abs(r);
+        }
+        //Метод нахождения площади окружности
+        static public double GetSquare(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        //Метод определения принадлежности точки к окружности
+        static public string GetAffiliation(double x, double y, double r)
+        {
+            if (Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2)) <= Math.Abs(r))
+                return "Точка с указанными координатами принадлежит окружности";
+            else
+                return "Точка с указанными координатами не принадлежит окружности";
+        }
+    }
+}
